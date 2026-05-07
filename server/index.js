73f1a815075c1fb470043b59490fcdcfea601569
vinayaka-112006 <v19468833@gmail.com>
2026-05-08 05:00:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,23 +2,36 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const authRoutes = require("./routes/authRoutes");
 const planRoutes = require("./routes/planRoutes");
 const geminiRoutes = require("./routes/geminiRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "https://physiopath-sandy.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173"
+];
+
 app.use(
   cors({
-
-    origin: [
-      "https://physiopath-sandy.vercel.app/",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
-
 );
+
+// Handle preflight requests
+app.options("*", cors());
+
 app.use(express.json());
 
 // Routes
